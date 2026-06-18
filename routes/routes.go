4e18ff0aa4c095a103bcac0f789fb817ed94c6e3
RafiBakhtiar23 <rafibakkhtiar@gmail.com>
2026-06-18 20:42:04 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -2,46 +2,34 @@ package routes
 
 import (
 	"jobconnect-api/controllers"
-
 	"github.com/gin-gonic/gin"
-
 	"jobconnect-api/middleware"
 )
 
 func SetupRoutes(router *gin.Engine) {
-
 	router.POST("/register", controllers.Register)
-
 	router.POST("/login", controllers.Login)
-
 	protected := router.Group("/")
-
 	protected.Use(middleware.AuthMiddleware())
-
 	{
 		protected.GET("/profile", func(c *gin.Context) {
 
 			c.JSON(200, gin.H{
 				"message": "Selamat datang, kamu sudah login",
 			})
-
 		})
 		protected.GET("/jobs", controllers.GetJobs)
-
 		protected.GET("/jobs/:id", controllers.GetJobByID)
-
 		protected.POST(
 			"/jobs",
 			middleware.AdminMiddleware(),
 			controllers.CreateJob,
 		)
-
 		protected.PUT(
 			"/jobs/:id",
 			middleware.AdminMiddleware(),
 			controllers.UpdateJob,
 		)
-
 		protected.DELETE(
 			"/jobs/:id",
 			middleware.AdminMiddleware(),
